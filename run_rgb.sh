3eea0de33faa6cc90037d63f9448d26b6019e501
hammad001 <hammadabdullahayyubi@gmail.com>
2019-05-30 06:48:20 +0000

    Corrected rgb list file in rgb run file

--- run_rgb.sh
+++ run_rgb.sh
@@ -1,1 +1,1 @@
-python main.py ucf101 RGB /$1/data/ucf101_flow_train_split_1.txt /$1/data/ucf101_flow_val_split_1.txt --arch BNInception --num_segments 3 --gd 20 --lr 0.001 --lr_steps 30 60 --epochs 80 -b 128 -j $2 --dropout 0.8 --snapshot_pref ucf101_bninception_ --gpus $3
+python main.py ucf101 RGB /$1/data/ucf101_rgb_train_split_1.txt /$1/data/ucf101_rgb_val_split_1.txt --arch BNInception --num_segments 3 --gd 20 --lr 0.001 --lr_steps 30 60 --epochs 80 -b 128 -j $2 --dropout 0.8 --snapshot_pref ucf101_bninception_ --gpus $3
